5ae186714ab5afac78e8c6a97fb28fd333882bc9
Tushar <tushar.kadam@auxilo.com>
2026-04-23 12:26:24 +0530

    Add WhatsappClient class for sending messages via WhatsApp with number formatting

---
+++ src/Channels/WhatsappClient.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dice\Channels;
+
+class WhatsappClient{
+    private $client;
+
+    public function __construct($baseClient){
+        $this->client = $baseClient;
+    }
+
+    public function send($template_id,$template_attr,$mobile_no,$source = 'SDK',$message_type = 'transactional'){
+        if (empty($mobile_no) || trim($mobile_no) == ''){
+            return false;
+        }
+
+        $cleanMobile = preg_replace('/\D/', '', $mobile_no);
+
+        if (empty($cleanMobile)){
+            return false;
+        }
+
+        if (strlen($cleanMobile) == 10) {
+            $formattedMobile = '91' . $cleanMobile;
+        } elseif (strlen($cleanMobile) == 12 && substr($cleanMobile, 0, 2) === '91') {
+            $formattedMobile = $cleanMobile;
+        } else {
+            $formattedMobile = '91' . substr($cleanMobile, -10);
+        }
+
+        $payload = [
+            'mobile_no' => $formattedMobile,
+            'channel' => 'whatsapp',
+            'source' => $source,
+            'type' => $message_type,
+            'template_id' => $template_id,
+            'template_attr' => $template_attr
+        ];
+
+        return $this->client->post($payload);
+    }
+}
